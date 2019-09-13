9d461d6a6bbbd8fa485a1710fa19bac2d9173d92
stsoward <darkarchonsc2@gmail.com>
2019-09-13 09:51:30 -0400

    Update CorrectResultOnInput.cs

--- FizzBuzz/CorrectResultOnInput.cs
+++ FizzBuzz/CorrectResultOnInput.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FizzBuzzTests
 {
     [TestClass]
-    public class CorrectResultOnInput
+    public class CorrectStringOnInput
     {
         StringList TestObj = new StringList();
         int input;
@@ -12,88 +12,88 @@ namespace FizzBuzzTests
         [TestMethod]
         public void Returns_1_on_1_input()
         {
-            //Arrange
-            input = 1;            
-            //Act
+            //Given I have a user input of 1
+            input = 1;
+            //When I call TestObj.NumList()
             var result = TestObj.NumList(input);
-            //Assert
+            //Then TestObj.NumList() will return 1
             Assert.AreEqual("1", result);
         }
 
         [TestMethod]
         public void Returns_correct_on_2_input()
         {
-            //Arrange
+            //Given I have a user input of 2
             input = 2;
-            //Act
+            //When I call TestObj.NumList()
             var result = TestObj.NumList(input);
-            //Assert
+            //Then TestObj.NumList() will return 1,2
             Assert.AreEqual("1,2", result);
         }
 
         [TestMethod]
         public void Returns_correct_on_3_input()
         {
-            //Arrange
+            //Given I have a user input of 3
             input = 3;
-            //Act
+            //When I call TestObj.NumList()
             var result = TestObj.NumList(input);
-            //Assert
+            //Then TestObj.NumList() will return a string ending in Fizz
             Assert.IsTrue(result.EndsWith("Fizz"));      
         }
 
         [TestMethod]
         public void Returns_correct_on_5_input()
         {
-            //Arrange
+            //Given I have a user input of 5
             input = 5;
-            //Act
+            //When I call TestObj.NumList()
             var result = TestObj.NumList(input);
-            //Assert
+            //Then TestObj.NumList() will return a string ending in Buzz
             Assert.IsTrue(result.EndsWith("Buzz"));
         }
 
         [TestMethod]
         public void Returns_correct_on_6_input()
         {
-            //Arrange
+            //Given I have a user input of 6
             input = 6;
-            //Act
+            //When I call TestObj.NumList()
             var result = TestObj.NumList(input);
-            //Assert
+            //Then TestObj.NumList() will return a string ending in Fizz
             Assert.IsTrue(result.EndsWith("Fizz"));
         }
 
         [TestMethod]
         public void Returns_correct_on_10_input()
         {
-            //Arrange
+            //Given I have a user input of 10
             input = 10;
-            //Act
+            //When I call TestObj.NumList()
             var result = TestObj.NumList(input);
-            //Assert
+            //Then TestObj.NumList() will return a string ending in Buzz
             Assert.IsTrue(result.EndsWith("Buzz"));
         }
 
         [TestMethod]
         public void Returns_correct_on_15_input()
         {
-            //Arrange
+            //Given I have a user input of 15
             input = 15;
-            //Act
+            //When I call TestObj.NumList()
             string result = TestObj.NumList(input);
-            //Assert
+            //Then TestObj.NumList() will return a string ending in FizzBuzz
             Assert.IsTrue(result.EndsWith("FizzBuzz"));
         }
 
         [TestMethod]
         public void Returns_correct_on_300_input()
         {
-            //Arrange
+            //Given I have a user input of 300
             input = 300;
-            //Act
+            //When I call TestObj.NumList()
             string result = TestObj.NumList(input);
-            //Assert
+            //Then TestObj.NumList() will return a string ending in FizzBuzz
             Assert.IsTrue(result.EndsWith("FizzBuzz"));
         }
     }
